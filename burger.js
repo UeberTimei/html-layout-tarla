31e1bdc7edc3f5e45bb882ae13b285a80f9450a3
UeberTimei <super.timati04@gmail.com>
2023-12-26 18:54:01 +0300

    Little fixes

--- burger.js
+++ burger.js
@@ -2,6 +2,7 @@
   const burger = document.querySelector(".burger");
   const menu = document.querySelector(".navigators");
   const menuClose = document.querySelector(".header-nav-close");
+  const menuCloseAdd = document.querySelector(".header-nav-close-line");
 
   burger.addEventListener("click", () => {
     menu.classList.add("navigators-active");
@@ -9,5 +10,18 @@
 
   menuClose.addEventListener("click", () => {
     menu.classList.remove("navigators-active");
+  });
+  menuCloseAdd.addEventListener("click", () => {
+    menu.classList.remove("navigators-active");
+  });
+})();
+
+(function () {
+  const post_office = document.querySelector(".post-office");
+  const check_button = document.querySelector(".post-office-button");
+
+  check_button.addEventListener("click", () => {
+    console.log("boba");
+    post_office.style.display = "none";
   });
 })();
